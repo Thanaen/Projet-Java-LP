cdfe9188fc8a598e5f4743bef70b7e28929fa945
Thanaen <Clem0970@gmail.com>
2017-10-20 17:15:18 +0200

    *travaux en cours*

---
+++ src/Interface.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Interface extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    //slt
+    // ceci est un test d'integration a github
+}
---
+++ src/Verrou.java
@@ -0,0 +1,18 @@
+
+public class Verrou {
+    private boolean etat; // True = ouvert False = ferm�
+    private boolean etatInitial;
+
+    public Verrou(boolean etatInitial) {
+        this.etatInitial = etatInitial;
+        this.etat = etatInitial;
+    }
+
+    public void ouvrir() {
+        this.etat = true;
+    }
+
+    public void fermer() {
+        this.etat = false;
+    }
+}
