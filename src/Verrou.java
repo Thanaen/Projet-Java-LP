3cd861d978a97bf4cbe54673d94d2ec1601c7d96
Bsoubrevilla <basil.soubrevilla@orange.fr>
2017-11-08 11:17:41 +0100

    Rajout de la fonction ChangerEtat

--- src/Verrou.java
+++ src/Verrou.java
@@ -3,15 +3,24 @@ public class Verrou {
     private boolean etat; // True = ouvert False = ferm�
     private boolean etatInitial;
 
+    // CONSTRUCTEUR
     public Verrou(boolean etatInitial) {
         this.etatInitial = etatInitial;
         this.etat = etatInitial;
     }
 
+    // Ferme le verrou s'il est ouvert, l'ouvre sinon
+    public void changerEtat() {
+		if(this.etat = false){ouvrir()}
+		else{fermer()}
+	}
+    
+    // Ouvre le verrou
     public void ouvrir() {
         this.etat = true;
     }
 
+    // Ferme le verrou
     public void fermer() {
         this.etat = false;
     }
