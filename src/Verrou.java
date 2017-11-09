ea3484dda4803178e3de699e00428605d265e6e6
Bsoubrevilla <basil.soubrevilla@orange.fr>
2017-11-09 16:49:24 +0100

    voila content ?

--- src/Verrou.java
+++ src/Verrou.java
@@ -1,29 +1,24 @@
 
 public class Verrou {
-    // Attributs
     private boolean etat; // True = ouvert False = ferm�
     private boolean etatInitial;
 
-    // CONSTRUCTEUR
     public Verrou(boolean etatInitial) {
         this.etatInitial = etatInitial;
         this.etat = etatInitial;
     }
-
-    // Ferme le verrou s'il est ouvert, l'ouvre sinon
-    public void changerEtat() {
-		if(this.etat = false){ouvrir();}
+	
+	public void changerEtat() {
+		if(this.etat == false){ouvrir();}
 		else{fermer();}
 	}
-    
-    // Ouvre le verrou
     public void ouvrir() {
         this.etat = true;
     }
 
-    // Ferme le verrou
     public void fermer() {
         this.etat = false;
     }
+    
+    public boolean getEtat(){return this.etat;}
 }
-
