d1c2833e7cf2aed0678779e508828f0c375c3aa0
lbarbe <barbe.loic710@gmail.com>
2017-11-10 22:10:46 +0100

    ca a modifier ca en ffaisant le merge m	is ca a rien changer

--- src/main.java
+++ src/main.java
@@ -3,7 +3,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		fenetreTest f = new fenetreTest("ma fenetre");
+		//fenetreTest2 f = new fenetreTest2("ma fenetre", 1);
+		fenetreTest2 f2 = new fenetreTest2("ma fenetre2", 2);
 	}
 
 }
