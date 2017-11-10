b40a32c4126edf6c8f6f4fd1224761cc006d8ac5
lbarbe <barbe.loic710@gmail.com>
2017-11-10 22:12:59 +0100

    pareil

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//fenetreTest2 f = new fenetreTest2("ma fenetre", 1);
-		fenetreTest2 f2 = new fenetreTest2("ma fenetre2", 2);
+		fenetreTest2 f2 = new fenetreTest2("ma fenetre2", 2); 
 	}
 
 }
